c43891051f8a2ca05794ae41514cd44d26b3f996
acassell26435 <nanicassell@gmail.com>
2023-11-05 18:54:20 -0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'https://example.com';
+    public string $baseURL = 'https://crm-bgsc.com';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
